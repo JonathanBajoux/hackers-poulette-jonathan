fcb01b1ffbeed8989701db205fe3a18ff85ffc61
jonathan <jonathanbajoux.pro@gmail.com>
2023-01-31 16:46:45 +0100

    jonathan

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>formulaire</title>
 </head>
-<body>
-    <form action="validation.php" method="post">
-    <label for="Name">Name</label>
-    <input type="text" name="Name" id="Name" required>
-    <label for="Firstname">Firstname</label>
-    <input type="text" name="Firstname" id="Firstname" required>
-    <label for="Email address">E-mail address</label>
-    <input type="email" name="Email" id="Email" required>
-    <label for="Comments">Comments</label>
-    <input type="text" name="Comments" id="Comments" required>
 
-    <input type="submit" value="confirm">
-</form>
+<body>
+    <div style="text-align: center;"> 
+    <h1>CONTACT</h1>
+    <form action="validation.php" method="post">
+        <label for="Name">Name</label><br>
+        <input type="text" name="Name" id="Name" required minlenght="2" maxlength="250"><br>
+        <label for="Firstname">Firstname</label><br>
+        <input type="text" name="Firstname" id="Firstname" required minlenght="2" maxlength="250"><br>
+        <label for="Email address">E-mail address</label><br>
+        <input type="email" name="Email" id="Email" required minlenght="2" maxlength="250"><br>
+        <label for="Comments">Comments</label><br>
+        <input type="text" name="Comments" id="Comments" required minlenght="250" maxlength="1000"><br><br>
+
+        <input type="submit" value="confirm">
+    </form>
+    </div>
 </body>
+
 </html>
--- validation.php
+++ validation.php
@@ -2,11 +2,43 @@
 // connexion à la base
 require_once("connection.php");
 
+
+
 if (empty($_POST['Name']) || empty($_POST['Firstname']) || empty($_POST['Email']) || empty($_POST['Comments'])) {
-    echo '<p class="erreur">Attention, vous devez remplir tous les champs</p>';
+    echo '<p class="Error">Attention, you must fill out all fields."</p>';
+} else {
+    $name = trim($_POST['Name']);
+    $name = strip_tags($name);
+    $name = htmlspecialchars($name);
+        if (!preg_match("/^[a-zA-Z]+$/",$_POST["Name"])) {
+            echo"The name is not valid, it must only contain alphanumeric characters.";
+    exit();
+        }
+    $firstname = trim($_POST['Firstname']);
+    $firstname = strip_tags($firstname);
+    $firstname = htmlspecialchars($firstname);
+        if (!preg_match("/^[a-zA-Z]+$/",$_POST["Firstname"])) {
+            echo"The firstname is not valid, it must only contain alphanumeric characters.";
+    exit();
+        }
+    $email = trim($_POST['Email']);
+    $email = strip_tags($email);
+    $email = htmlspecialchars($email);
+        if (!filter_var($_POST["Email"], FILTER_VALIDATE_EMAIL)){
+            echo"The email address is not valid.";
+    exit();
+        }
+    $comments = trim($_POST['Comments']);
+    $comments = strip_tags($comments);
+    $comments = htmlspecialchars($comments);
+        if (!preg_match("/^[a-zA-Z0-9 .,_\/-]+$/", $_POST["Comments"])) {
+            echo '<p class="error">Comments can only contain alphanumeric characters, periods, commas, dashes and slashes</p>';
+        exit();
+    }
 
     // on écrit la requête sql
-    $sql = 'INSERT INTO contactformulaire(Name, Firstname, Email, Comments) VALUES(' . $bdd->quote($_POST['Name']) . ','
+    $sql = 'INSERT INTO contactformulaire(Name, Firstname, Email, Comments) VALUES(' 
+        . $bdd->quote($_POST['Name']) . ','
         . $bdd->quote($_POST['Firstname']) . ','
         . $bdd->quote($_POST['Email']) . ','
         . $bdd->quote($_POST['Comments']) . ')';
@@ -17,7 +49,12 @@ if (empty($_POST['Name']) || empty($_POST['Firstname']) || empty($_POST['Email']
     } catch (Exception $e) {
         echo '<p class="erreur">', $e->getMessage(), '</p>';
     }
+    
     // on affiche le résultat pour le visiteur
-    echo '<span class="laclasse qui va bien">Votre message à bien été envoyé !</span>';
-}
+    echo '<span class="the class that is doing well">Your message has been sent !</span>';
+
+ header("Location: index.php");
+exit;   
+    }
+
 ?>
